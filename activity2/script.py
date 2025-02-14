dedbff27ad399fe790eee7eadb33bd2e30a4f5d1
ChunkGlow <memoguy34@gmail.com>
2025-02-14 17:45:45 +0800

    Python2

---
+++ activity2/script.py
@@ -0,0 +1,37 @@
+#Print a Sentence
+
+print("You Have Entered this Command")
+
+#Print a Number
+
+print("2.5.5.5.5.")
+
+#Next line after Print
+
+print("Gateway \n")
+
+#Pass multiple argumets for printing
+
+print("Gateway",342200)
+
+#End
+
+print("Welcome to", end="FPortal")
+
+#//
+
+a = input("Type In A Word and It will Display   :    ")
+
+print(a)
+
+x = 25; 
+
+y = "Selector"
+
+print(x)
+
+print(y)
+
+import keyword
+
+print(keyword.kwlist)
