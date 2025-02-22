87ae7ee05082cc4f171a12e82dfed0cfbd8d5cfa
ChunkGlow <memoguy34@gmail.com>
2025-02-22 17:57:03 +0800

    Activity4

---
+++ activity5/script2.py
@@ -0,0 +1,15 @@
+a = int(input("Enter Cost Price:  "))
+b = int(input("Enter Selling Price :"))
+
+x = a - b
+
+if a < b:
+    print("Profit is:", x)
+elif  a > b:
+    print("Loss is:", x)
+else:
+ 
+  print("No Profit No Loss")
+
+  
+
